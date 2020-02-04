fed8b8fdcaaf71ca88c02b0c24da98761a563f7f
JerJohnston <59575963+JerJohnston@users.noreply.github.com>
2020-02-04 11:21:33 -0700

    Update SchoolTranscript.sql

--- DDL/SchoolTranscript.sql
+++ DDL/SchoolTranscript.sql
@@ -4,8 +4,10 @@
 *
 * CREATE DATABASE SchoolTranscript
 ****************** */
+
 USE SchoolTranscript
 GO
+
 /* === Drop Statements === */
 IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'StudentCourses')
     DROP TABLE StudentCourses
@@ -17,7 +19,9 @@ IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Students'
 /* === Create Tables === */
 CREATE TABLE Students
 (
-    StudentID       int             NOT NULL, 
+    StudentID       int
+        CONSTRAINT PK_Students_StudentID
+            PRIMARY KEY             NOT NULL, 
     GivenName       varchar(50)     NOT NULL,
     Surname         varchar(50)     NOT NULL,
     DateOfBirth     datetime        NOT NULL,
@@ -26,22 +30,33 @@ CREATE TABLE Students
 
 CREATE TABLE Courses
 (
-    Number      varchar(10)         NOT NULL,
-    [Name]      varchar(50)         NOT NULL,
-    Credits     decimal(3,1)        NOT NULL,
-    [Hours]     tinyint             NOT NULL,
-    Active      bit                 NOT NULL,
-    Cost        money               NOT NULL
+    Number          varchar(10)
+        CONSTRAINT PK_Courses_Number
+            PRIMARY KEY                 NOT NULL,
+    [Name]          varchar(50)         NOT NULL,
+    Credits         decimal(3,1)        NOT NULL,
+    [Hours]         tinyint             NOT NULL,
+    Active          bit                 NOT NULL,
+    Cost            money               NOT NULL
 )
 
 CREATE TABLE StudentCourses
 (
-    StudentID       int                 NOT NULL,
-    CourseNumber    varchar(10)         NOT NULL,
+    StudentID       int
+        CONSTRAINT FK_StudentCourses_StudentID_Students_StudentID
+             FOREIGN KEY REFERENCES Students(StudentID)
+                                        NOT NULL,
+    CourseNumber    varchar(10)
+        CONSTRAINT FK_StudentCourses_CourseNumber_Courses_Number
+            FOREIGN KEY REFERENCES Courses(Number)     
+                                        NOT NULL,
     [Year]          tinyint             NOT NULL,
     Term            char(3)             NOT NULL,
-    FinalMark       tinyint                 Null,
-    [Status]        char(1)             NOT NULL
+    FinalMark       tinyint                 NULL,
+    [Status]        char(1)             NOT NULL,
+    -- Table-level constraint for composite keys
+    CONSTRAINT PK_StudentCourses_StudentID_CourseNumber
+        PRIMARY KEY (StudentID, CourseNumber)
 )
 
 -- table definition is a set of column declarations
